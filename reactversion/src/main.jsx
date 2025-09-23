6c1730851fd680b0883c5df5e657fec501f077d1
urbruder <harshsingh310105@gmail.com>
2025-09-23 10:17:33 +0530

    Done

--- reactversion/src/main.jsx
+++ reactversion/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; // <-- Import
+import { AuthProvider } from './context/AuthContext.jsx'; // <-- Import
 import App from './App.jsx';
 import './index.css';
 import '@tensorflow/tfjs-backend-webgl';
@@ -16,3 +16,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 );
 // hlw
+//hii
